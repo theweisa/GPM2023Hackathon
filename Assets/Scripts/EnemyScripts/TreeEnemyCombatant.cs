090cd7da5963b22d8f96a35065341c1d121f4883
Andrew Mangal <amangal@ucsc.edu>
2023-11-12 01:24:32 -0800

    Tree Enemy Logic Implemented
    (prefab needs to be updated so it actually moves)
    (gameSpawner logic needs to be updated so they are spawned)

--- Assets/Scripts/EnemyScripts/TreeEnemyCombatant.cs
+++ Assets/Scripts/EnemyScripts/TreeEnemyCombatant.cs
@@ -4,14 +4,39 @@ using UnityEngine;
 
 public class TreeEnemyCombatant : EnemyCombatant
 {
+    /*
+    Vector2 moveDirection;
+    public float energyYield;
+    public float expYield;
+    public GameObject expDrop;
+    public float movementMultiplier = 1000f;
+    public Transform target;
+    */
+
+    protected override void Start() {
+        base.Start();
+        target = SelectTarget();        
+    }
+
+    public Transform SelectTarget(){
+        //get Tree Parent Object (from GameManager)
+        int size = InstantiationManager.Instance.treeParent.transform.childCount;
+        if(size != 0){
+            //choose random child out of parent.numOfChildren
+            return InstantiationManager.Instance.treeParent.transform.GetChild(Random.Range(0, size));
+        }
+        else{
+            //return Player transform
+            return PlayerManager.Instance.transform;
+        }
+    }
+
     protected override void FixedUpdate()
     {
         base.FixedUpdate();
-        print("we made it");
-    }
-    public override void MoveCombatant()
-    {
-        base.FixedUpdate();
-        print("we made it");
+        //if there's no current target selected, then select a new one
+        if(!target){
+            target = SelectTarget();
+        }
     }
 }
